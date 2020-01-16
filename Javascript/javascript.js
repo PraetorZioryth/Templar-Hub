211584978c5274025a48b81dcea39ecb3685b0a4
Skirts Brief <33298896+SkirtsBrief@users.noreply.github.com>
2020-01-15 19:49:15 -0500

    first commit

---
+++ Javascript/javascript.js
@@ -0,0 +1,10 @@
+/*-----< Top Navigation Scripts >-----*/
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+/*------------------------------------*/
